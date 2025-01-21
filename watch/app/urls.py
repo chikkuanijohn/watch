530686ad47a887e6b7b5f251c1e4e5f5fc21ca06
chikkuanijohn <chikkuanijohn1@gmail.com>
2025-01-21 19:52:04 +0530

    new file

---
+++ watch/app/urls.py
@@ -0,0 +1,34 @@
+from django.urls import path
+from .import views
+
+
+urlpatterns = [
+    path('',views.shop_login),
+    
+    #------------admin------------#
+    path('shop_home',views.shop_home),
+    path('logout',views.shop_logout),
+    path('addproduct', views.addproduct),
+    path('edit_product/<pid>',views.edit_product),
+    path('delete_product/<pid>',views.delete_product),
+    path('add_cate',views.add_category),
+    path('view_bookings',views.view_bookings),
+    
+    
+    #------------user--------------#
+    path('register/',views.register),
+    path('user_home',views.user_home),
+    path('contact/', views.contact),
+    path('about_us', views.about_us),
+    path('product_view/<pid>',views.product_view),
+    path('add_to_cart/<pid>',views.add_to_cart),
+    path('view_cart',views.view_cart),
+    path('cart_pro_buy/<cid>',views.cart_pro_buy),
+    path('bookings',views.bookings),
+    path('pro_buy/<pid>',views.pro_buy),
+    path('qty_in/<cid>',views.qty_in),
+    path('qty_dec/<cid>',views.qty_dec),
+    path('category_view/<int:category_id>/', views.category_view, name='category_view'),
+    
+
+]
